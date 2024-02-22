97d7674a04970ce1df65d0ad8141d17714544fd7
Bartosz <bartos.pindel@gmail.com>
2024-02-22 01:06:45 +0100

    add about tsx, small fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-
 const About = () => {
-  return <div>Small section for my background</div>;
+  const divStyle = "w-5/6 mx-auto text-center";
+
+  return (
+    <div className="h-screen font-headlines flex flex-col">
+      <h4 className="text-3xl text-center text-primary border-b-4 border-primary w-4/5 mx-auto">
+        Personal details
+      </h4>
+      <div className="h-full flex flex-col justify-evenly">
+        <div className={`${divStyle}`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+          doloremque, dicta at eligendi repellendus eum nisi sunt. Repellendus
+          odio necessitatibus, dicta ratione assumenda corporis beatae ex
+          doloremque perspiciatis quis rerum?
+        </div>
+        <div className={`${divStyle}`}>
+          Exercitationem quibusdam quisquam illo sapiente aperiam alias
+          dignissimos itaque tempore, amet atque sit a modi nesciunt quae,
+          aliquam ab debitis unde recusandae hic id officiis eius ratione
+          numquam voluptatibus. Earum.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,7 +3,7 @@ import Slides from "./Slides";
 const Projects = () => {
   return (
     <div className="h-screen flex flex-col">
-      <h3 className="text-2xl text-center p-3 uppercase">Projects:</h3>
+      <h3 className="text-2xl text-center p-3 uppercase">Equipment:</h3>
       <Slides />
     </div>
   );
